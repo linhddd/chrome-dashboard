455b17c1b95097bdfe2197f0192810db3f42b996
Linh Huyen Dieu <ldie@hellofresh.com>
2022-08-12 14:34:31 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+    .then(res => res.json())
+    .then(data => {
+        document.body.style.backgroundImage = `url(${data.urls.regular})`
+		document.getElementById("author").textContent = `By: ${data.user.name}`
+    })
+    .catch(err => {
+        // Use a default background image/author
+        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
+)`
+		document.getElementById("author").textContent = `By: Dodi Achmad`
+    })
+
+fetch("https://api.frankfurter.app/latest?to=USD,SGD")
+    .then(res => {
+        if(!res.ok) {
+            throw Error("Uh-oh!! Please try again later")
+        }
+        return res.json()
+    })
+    .then(data => {
+        console.log(data.date)
+        console.log(data.rates.SGD)
+        console.log(data.rates.USD)
+        document.getElementById("rates").innerHTML = `
+        <p>💰💰 Exchange Rates 💰💰</p>
+        <p class="rates-date">📌📌 ${data.date}</p>
+        <p class="rates-to">💶 ⏳ $: ${data.rates.USD}</p>
+        <p class="rates-to">💶 ⏳ S$: ${data.rates.SGD}</p>
+        `
+    })
+    .catch(err => console.error(err))
+
+function getCurrentTime() {
+    const date = new Date()
+    document.getElementById("time").textContent = date.toLocaleTimeString("en-us", {timeStyle: "medium"})
+}
+
+setInterval(getCurrentTime, 1000)
+
+navigator.geolocation.getCurrentPosition(position => {
+    fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`)
+        .then(res => {
+            if (!res.ok) {
+                throw Error("Weather data not available")
+            }
+            return res.json()
+        })
+        .then(data => {
+            const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+            document.getElementById("weather").innerHTML = `
+                <img src=${iconUrl} />
+                <p class="weather-temp">${Math.round(data.main.temp)}º</p>
+                <p class="weather-city">${data.name}</p>
+            `
+        })
+        .catch(err => console.error(err))
+});
